fd1477b7aade8373ec4c9c02f7059b38653856d0
Nascutiu Claudiu <nascutiu.claudiu@gmail.com>
2022-05-30 21:14:07 +0300

    First Commit

---
+++ src/main/java/com/example/barbershopprogramming/BarberShopProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.example.barbershopprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberShopProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarberShopProgrammingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/barbershopprogramming/BarberShopProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barbershopprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberShopProgrammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
