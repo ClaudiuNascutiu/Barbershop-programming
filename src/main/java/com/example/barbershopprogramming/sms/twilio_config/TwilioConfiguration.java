44477d98b64c340a6eacc2c8d599f37b771e9b04
Nascutiu Claudiu <nascutiu.claudiu@gmail.com>
2022-10-29 18:15:02 +0300

    ....

--- src/main/java/com/example/barbershopprogramming/sms/twilio_config/TwilioConfiguration.java
+++ src/main/java/com/example/barbershopprogramming/sms/twilio_config/TwilioConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 @Setter
 public class TwilioConfiguration {
 
-    private final String accountSid = "ACb61de0e8dfd08a82092cd797eae9bc40";
-    private final String authToken = "0604e0399552b5c21929dce8062b074f";
+    private final String accountSid = "";
+    private final String authToken = "";
     private final String trialNumber = "+18434387133";
 }
